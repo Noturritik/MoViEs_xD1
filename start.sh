dc8120d7727feddcad4e97620d71ba79cda6003b
Noturritik <122246719+Noturritik@users.noreply.github.com>
2023-01-12 11:51:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/LazyDeveloperr/LazyPrincessV2 /LazyPrincessV2
+  git clone https://github.com/Noturritik/MoViEs_xD1 /MoViEs_xD1 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincessV2
+  git clone $UPSTREAM_REPO /MoViEs_xD1
 fi
-cd /LazyPrincessV2
+cd /MoViEs_xD1
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
